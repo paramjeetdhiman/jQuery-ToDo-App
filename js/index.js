ac790d490fc7a56cfcdacdc8bbdb934a9b015506
Paramjeet Dhiman <paramjeetdhiman1997@gmail.com>
2020-09-18 00:31:59 +0530

    ADD: func add or delete

--- js/index.js
+++ js/index.js
@@ -1,30 +1,26 @@
 //check off specific todos
-$('li').on('click', function () {
-  $(this).toggleClass('completed');
-  //   if ($(this).css('color') === 'rgb(128, 128, 128)') {
-  //     $(this).css({
-  //       color: 'black',
-  //       'text-decoration': 'none',
-  //     });
-  //   } else {
-  //     $(this).css({
-  //       'text-decoration': 'line-through',
-  //       color: 'gray',
-  //     });
-  //   }
-});
 
-$('span').on('click', function (e) {
+//add listeners to parent or which exists on page when load but we are really only listening to the li that are clicked inside the ul
+
+$('ul').on('click', 'li', function () {
+  $(this).toggleClass('completed'); 
+});
+  
+$('ul').on('click', 'span', function (e) {
   $(this)
     .parent()
     .fadeOut(500, function () {
       $(this).remove();
-    });
+    }); 
   e.stopPropagation(); //it prevents us from event bubbling up...
 });
 
 $("input[type='text']").on('keypress', function (e) {
-  if (e.which === '13') {
-    $(this).val();
+  if (e.which === 13) {
+    //grabbing new todo text from input
+    const todoText = $(this).val();
+    //create a new li and add to ul
+    $('ul').append(`<li><span>X</span> ${todoText}</li>`);
+    $(this).val(''); //setter
   }
 });
