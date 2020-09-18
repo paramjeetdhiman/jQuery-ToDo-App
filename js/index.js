661351a34fb754192d963b03a804ae6bcbf44392
Paramjeet Dhiman <paramjeetdhiman1997@gmail.com>
2020-09-18 12:56:53 +0530

    ADD: styling done

--- js/index.js
+++ js/index.js
@@ -3,24 +3,33 @@
 //add listeners to parent or which exists on page when load but we are really only listening to the li that are clicked inside the ul
 
 $('ul').on('click', 'li', function () {
-  $(this).toggleClass('completed'); 
+  $(this).toggleClass('completed');
 });
-  
+
 $('ul').on('click', 'span', function (e) {
   $(this)
     .parent()
     .fadeOut(500, function () {
       $(this).remove();
-    }); 
+    });
   e.stopPropagation(); //it prevents us from event bubbling up...
 });
 
 $("input[type='text']").on('keypress', function (e) {
   if (e.which === 13) {
-    //grabbing new todo text from input
-    const todoText = $(this).val();
-    //create a new li and add to ul
-    $('ul').append(`<li><span>X</span> ${todoText}</li>`);
-    $(this).val(''); //setter
+    if ($(this).val() !== '') {
+      //grabbing new todo text from input
+      const todoText = $(this).val();
+      //create a new li and add to ul
+      $('ul').append(
+        `<li><span><i class="fa fa-trash"></i></span>${todoText}</li>`
+      );
+      $(this).val(''); //setter
+    }
+  }
+});
+
+$('.fa-plus').on('click', function () {
+  if ($("input[type='text']").fadeToggle()) {
   }
 });
